472a88a0d276793c31b02820d0599a27ebc7c1e8
nmanjos <nmanjos@gmail.com>
2017-02-06 10:06:14 +0000

    started

---
+++ NetworkSim/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkSim
+{
+    class Program
+    {
+        public static int Dec2bin(int dec)
+        {
+            int bin=0;
+            return bin;
+        }
+        public static int Bin2dec(int bin)
+        {
+            int dec = 0;
+            return dec;
+        }
+        public static int encoder(int bin)
+        {
+
+        }
+        public static int decoder(int bin)
+        {
+
+        }
+        public static Boolean Netsend()
+        {
+
+        }
+        public static Boolean Netrecieve()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
